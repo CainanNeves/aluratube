fee3e32975dde878e9cc9b03d5c223c662c17294
CainanNeves <cainan.lima@ufba.br>
2022-11-22 18:50:14 -0300

    minor ajustments

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import tubeImg from "../assets/tube.svg";
+import { FaYoutube } from "react-icons/fa";
 
 export default function Logo() {
   return (
     <div className="flex h-6 w-full max-w-[80px] font-bold text-textColorBase sm:max-w-[127px]">
-      <Image src={tubeImg} alt="AluraTube" />
+      <FaYoutube color="red" className="h-full w-full p-0" />
       <span>NaniacTube</span>
     </div>
   );
